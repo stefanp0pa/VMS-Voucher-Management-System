e06e9d8b0b3a53232e9e00516c959023fcc57033
AndreiPopa21 <andrei.popa2199@gmail.com>
2019-12-26 21:44:08 +0200

    Implemented User class

--- VMS/src/com/company/User.java
+++ VMS/src/com/company/User.java
@@ -1,4 +1,30 @@
 package com.company;
 
+import java.util.List;
+
 public class User {
+
+    public enum UserType{
+        ADMIN,GUEST
+    }
+
+    private Integer userId;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+    private UserType userType;
+
+    private UserVoucherMap receivedVouchers;
+    private List<Notification> userNotifications;
+
+    public Integer getUserId(){return this.userId;}
+    public String getUserName(){return  this.userName;}
+    public String getUserEmail(){return  this.userEmail;}
+    public String getUserPassword(){return  this.userEmail;}
+    public UserType getUserType(){return this.userType;}
+
+    public void update(Notification notification){
+        userNotifications.add(notification);
+    }
+
 }
