7f3d9ef4f8af421a9fc63196a3081d80a6754ef1
AndreiPopa21 <andrei.popa2199@gmail.com>
2019-12-26 21:40:43 +0200

    Implemented VMS class using Singleton pattern

--- VMS/src/com/company/VMS.java
+++ VMS/src/com/company/VMS.java
@@ -1,4 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class VMS {
+
+    private static VMS vmsInstance = null;
+
+    private VMS(){
+
+    }
+
+    public static VMS getInstance(){
+        if(VMS.vmsInstance == null){
+            VMS.vmsInstance = new VMS();
+        }
+        return VMS.vmsInstance;
+    }
+
+    private Vector<Campaign> campaigns;
+    private Vector<User> users;
+
+    public Vector<Campaign> getCampaigns(){return this.campaigns;}
+    public Campaign getCampaign(Integer id){return null;}
+    public void addCampaign(Campaign campaign){this.campaigns.add(campaign);}
+    public void updateCampaign(Integer id, Campaign campaign){}
+    public void cancelCampaign(Integer id){}
+
+    public Vector<User> getUsers(){return this.users;}
+    public void addUser(User user){this.users.add(user);}
+
+
 }
